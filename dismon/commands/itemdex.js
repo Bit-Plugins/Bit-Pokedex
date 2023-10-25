2f0fa755327815d5cdc022fe4700e4294b0cf014
Robin P <lochlanpainter@gmail.com>
2023-10-25 13:22:30 +1100

    Added Itemdex

---
+++ dismon/commands/itemdex.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder, ButtonBuilder, EmbedBuilder, Permission, MessageButton, AttachmentBuilder  } = require('discord.js');
+const Pokedex = require('pokedex-promise-v2');
+var P = new Pokedex();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('itemdex')
+        .setDescription('Get information on a specific item')
+        .setDMPermission(false)
+        .addStringOption((option) => 
+            option.setName('item')
+            .setDescription('The item you want information on.')
+            .setRequired(true)
+        ),
+    async execute(interaction) {
+        const item = interaction.options.getString('item').toLowerCase();
+            P.getItemByName(item.toLowerCase().replaceAll(" ", "-"))
+                .then(function(response) {
+                const pokeEmbed = new EmbedBuilder()
+                    .setTitle('Itemdex | '+item)
+                    .setThumbnail(response.sprites.default)
+                    if(response.cost === 0) {
+                        pokeEmbed.addFields({ name: 'Cost in Mart', value: 'Not Purchaseable', inline: true })
+                    } else {
+                        pokeEmbed.addFields({ name: 'Cost in Mart', value: response.cost.toString(), inline: true })
+                    }
+                    pokeEmbed.addFields([
+                        { name: 'Effect', value: response.effect_entries[0].short_effect, inline: true },
+                        { name: 'Item Category', value: response.category.name.replaceAll("-", " "), inline: true }
+                    ])
+                    pokeEmbed.setDescription(response.flavor_text_entries[0].text.toString())
+                interaction.reply({ embeds: [pokeEmbed] })
+                return;
+            }) .catch((err) => {
+                //console.log(err)
+                if(err.response.status === 404) {
+                    return interaction.reply({
+                        content: "Item not found!"
+                    })
+                } else {
+                    interaction.reply({
+                        content: err.response.statusText
+                    })
+
+                    console.log("Unhandled Error "+err.response.status.toString()+" "+err.response.statusText)
+                    return;
+                }
+            });
+    }
+}
